d5ed7cbf44c915029bb68938059e53fc0fdeeb6c
mcdogg17 <makdog45@mail.ru>
2020-11-21 19:29:40 +0500

    random yellow circles

---
+++ UI.py
@@ -0,0 +1,30 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(846, 688)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Paint yellow circle"))
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import sys
 from random import randint
 
-from PyQt5 import uic, QtGui
-from PyQt5.QtCore import Qt
+
+from PyQt5 import QtGui
 from PyQt5.QtGui import QPainter
 from PyQt5.QtWidgets import QApplication, QWidget
+from UI import Ui_Form
 
 
-class Window(QWidget):
+class Window(QWidget, Ui_Form):
     def __init__(self):
         super(Window, self).__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.qp = QPainter()
         self.pushButton.clicked.connect(self.paint)
 
@@ -20,7 +21,7 @@ class Window(QWidget):
     def paintEvent(self, e: QtGui.QPaintEvent):
         self.qp.begin(self)
         d = randint(3, 100)
-        self.qp.setBrush(Qt.yellow)
+        self.qp.setBrush(QtGui.QColor(randint(0, 0xffffff)))
         self.qp.drawEllipse(randint(0, self.label.width() - d),
                             randint(0, self.label.height() - d),
                             d, d)
